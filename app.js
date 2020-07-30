40dbf1f91dcf4fd354f07c9a08a1e0afe9b0b937
Jeremy Ludwig <hjeremyludwig@gmail.com>
2020-07-30 14:47:48 -0400

    Completed JS functionality

---
+++ app.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('.controls input');
+
+inputs.forEach( input => {
+    input.addEventListener('change', (e) => {      
+        handleChange(e.target);
+    })
+})
+
+let handleChange = (input) => {
+    const root = document.querySelector(':root');
+    let name = input.getAttribute('name');
+    let suffix;
+    if (input.getAttribute('data-suffix') === null) {
+        suffix = '';
+    } else {
+        suffix = input.getAttribute('data-suffix');
+    }
+    root.style.setProperty(`--${name}`, `${input.value}${suffix}`);
+}
