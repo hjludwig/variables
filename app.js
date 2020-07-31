868cce5a71667ca075ee59e0d96595e92ad40b9e
Jeremy Ludwig <hjeremyludwig@gmail.com>
2020-07-31 09:52:15 -0400

    Minor changes

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const inputs = document.querySelectorAll('.controls input');
 
 inputs.forEach( input => {
-    input.addEventListener('change', (e) => {      
-        handleChange(e.target);
-    })
+    input.addEventListener('change', e => handleChange(e.target));
+    input.addEventListener('mousemove', e => handleChange(e.target));
 })
 
 let handleChange = (input) => {
     const root = document.querySelector(':root');
     let name = input.getAttribute('name');
-    let suffix;
-    if (input.getAttribute('data-suffix') === null) {
-        suffix = '';
-    } else {
-        suffix = input.getAttribute('data-suffix');
-    }
+    let suffix = input.getAttribute('data-suffix') || '';
     root.style.setProperty(`--${name}`, `${input.value}${suffix}`);
 }
+
